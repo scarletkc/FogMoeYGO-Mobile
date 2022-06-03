e6c98268c357892174aac266fca2682f837c7d36
Kc <kc@fog.moe>
2022-06-03 21:20:11 +0800

    update

--- mobile/src/main/java/cn/garymb/ygomobile/ui/preference/YGOPreferencesProviderFogMoe.java
+++ mobile/src/main/java/cn/garymb/ygomobile/ui/preference/YGOPreferencesProviderFogMoe.java
@@ -4,7 +4,7 @@ import com.zlm.libs.preferences.PreferencesProvider;
 
 import cn.garymb.ygomobile.App;
 
-public class YGOPreferencesProvider extends PreferencesProvider {
+public class YGOPreferencesProviderFogMoe extends PreferencesProvider {
     @Override
     public String getAuthorities() {
         return App.get().getPackageName() + ".ui.preference.YGOPreferencesProvider";
